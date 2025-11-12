97a00aaf4af7f2b7f4f62aca33e802de98c40dda
Kaishya Desai <kaishya@mac.wlan.dur.ac.uk>
2025-11-12 16:15:20 +0000

    Create Board Visual

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
-//#include <bits/stdc++.h>
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+using namespace std;
 
-const int M = 129;  // 1 + 2^7
-const int N = 9;    // 1 + 2^3
+const int M = 6;
+const int N = 7;
 
-void initBoard(int board[M][N], int M, int N) {
+void initBoard(int board[M][N]) {
   for (int m = 0; m < M; m++) {
     for (int n = 0; n < N; n++) {
       board[m][n] = 0;
@@ -14,58 +14,80 @@ void initBoard(int board[M][N], int M, int N) {
   }
 }
 
+void displayBoard(int board[M][N]) {
+  cout << "\nBoard:\n";
+  for (int m = 0; m < M; m++) {
+    for (int n = 0; n < N; n++) {
+      cout << "|";
+      if (board[m][n] == 0)
+        cout << " ";
+      else
+        cout << board[m][n];
+    }
+    cout << "|" << endl;
+  }
+  for (int n = 0; n < N; n++) cout << "--";
+  cout << "-\n";
+}
+
 int parseBoard(int board[M][N]) {
   /*
-   * This function is where you should define your win conditions.
-   * If player p in [1,...,P] has won, then you should return p; otherwise 0.
-   * If you find yourself with a great deal of spare time you might try
-   * rendering the board to the terminal here, too.
-   */
-  
+     * This function is where you should define your win conditions.
+     * If player p in [1,...,P] has won, then you should return p; otherwise 0.
+     * If you find yourself with a great deal of spare time you might try
+     * rendering the board to the terminal here, too.
+     */
   return 0;
 }
 
-void gameLoop(int board[M][N], bool &cont, int players, int M, int N) {
-  int winner;
-  int m, n;
-  do {
-    for (int p = 1; p <= players; ++p) {
-      std::cout << "Player " << p << " place:";
-      std::cin >> m >> n;
+void gameLoop(int board[M][N], bool cont, int players) {
+  int winner = 0;
+  int row, col;
 
-      // Add boundary checking
-      if (m < 0 || m >= M || n < 0 || n >= N) {
-        std::cout << "Out of bounds!" << std::endl;
-        --p;  // Retry this player's turn
+  while (cont) {
+    displayBoard(board);
+    for (int p = 1; p <= players; p++) {
+      cout << "Player " << p << " enter row and column (1-"
+           << M << " " << N << "): ";
+      cin >> row >> col;
+      row -= 1;
+      col -= 1;
+
+      if (row < 0 || row >= M || col < 0 || col >= N) {
+        cout << "Out of bounds, try again!\n";
+        p--;
         continue;
       }
 
-      if (board[m][n] == 0) {
-        std::cout << p << " at (" << m << ", " << n << ")." << std::endl;
-        board[m][n] = p;
-      } else {
-        std::cout << "Illegal move!" << std::endl;
-        --p;  // Retry this player's turn
+      if (board[row][col] != 0) {
+        cout << "That spot is already taken!\n";
+        p--;
+        continue;
+      }
+
+      board[row][col] = p;
+
+      winner = parseBoard(board);
+      if (winner != 0) {
+        displayBoard(board);
+        cout << "Player " << winner << " wins!\n";
+        cont = false;
+        break;
       }
     }
-    winner = parseBoard(board);
-    if (winner != 0) {
-      std::cout << "Player " << winner << " has won!" << std::endl;
-      cont = false;
-    }
-  } while (cont);
+  }
 }
 
-int main() {
-  int P;
+int main(void) {
+  int P = 2;
   bool cont = true;
 
-  std::cout << "Initialising game... How many players?";
-  std::cin >> P;
+  cout << "Connect 4\n";
 
   int board[M][N];
-  initBoard(board, M, N);
-  gameLoop(board, cont, P, M, N);
+  initBoard(board);
+  gameLoop(board, cont, P);
 
-  return EXIT_SUCCESS;
+  cout << "Game over.\n";
+  return 0;
 }
