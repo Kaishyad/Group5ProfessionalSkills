28794c8333761f898c102036056d00704ddb7635
mufeiy <112385036+mufeiy@users.noreply.github.com>
2025-11-13 10:12:28 +0000

    Refactor parseBoard function and tests for constants

--- tests/test_parseBoard.cpp
+++ tests/test_parseBoard.cpp
@@ -1,59 +1,95 @@
 #include "acutest.h"
 #include <iostream>
 
-extern int parseBoard(int board[6][7], int rows, int cols, bool winPositions[6][7]);
+#define M 6
+#define N 7
+
+int parseBoard(int board[M][N], int rows, int cols, bool winPositions[M][N] = nullptr) {
+    for (int m = 0; m < rows; m++) {
+        for (int n = 0; n < cols; n++) {
+            if (board[m][n] == 0) continue;
+
+            const int player = board[m][n];
+            constexpr int directions[4][2] = {{0, 1}, {1, 0}, {1, 1}, {1, -1}};
+            for (int d = 0; d < 4; d++) {
+                const int dm = directions[d][0];
+                const int dn = directions[d][1];
+                bool win = true;
+
+                for (int i = 1; i < 4; i++) {
+                    const int newM = m + dm * i;
+                    const int newN = n + dn * i;
+                    if (newM < 0 || newM >= rows || newN < 0 || newN >= cols || board[newM][newN] != player) {
+                        win = false;
+                        break;
+                    }
+                }
+
+                if (win) {
+                    if (winPositions) {
+                        for (int i = 0; i < 4; i++) {
+                            winPositions[m + dm * i][n + dn * i] = true;
+                        }
+                    }
+                    return player;
+                }
+            }
+        }
+    }
+    return 0;
+}
 
 void test_no_win(void) {
-    int board[6][7] = {0};
-    bool winPositions[6][7] = {false};
-    int winner = parseBoard(board, 6, 7, winPositions);
+    int board[M][N] = {0};
+    bool winPositions[M][N] = {false};
+    int winner = parseBoard(board, M, N, winPositions);
     TEST_CHECK(winner == 0); 
 }
 
 void test_horizontal_win(void) {
-    int board[6][7] = {0};
-    bool winPositions[6][7] = {false};
+    int board[M][N] = {0};
+    bool winPositions[M][N] = {false};
     board[0][0] = 1;
     board[0][1] = 1;
     board[0][2] = 1;
     board[0][3] = 1;
-    int winner = parseBoard(board, 6, 7, winPositions);
+    int winner = parseBoard(board, M, N, winPositions);
     TEST_CHECK(winner == 1);
 }
 
 void test_vertical_win(void) {
-    int board[6][7] = {0};
-    bool winPositions[6][7] = {false};
+    int board[M][N] = {0};
+    bool winPositions[M][N] = {false};
     board[0][0] = 2;
     board[1][0] = 2;
     board[2][0] = 2;
     board[3][0] = 2;
-    int winner = parseBoard(board, 6, 7, winPositions);
+    int winner = parseBoard(board, M, N, winPositions);
     TEST_CHECK(winner == 2);
 }
 
 void test_diagonal_win(void) {
-    int board[6][7] = {0};
-    bool winPositions[6][7] = {false};
+    int board[M][N] = {0};
+    bool winPositions[M][N] = {false};
     board[0][0] = 1;
     board[1][1] = 1;
     board[2][2] = 1;
     board[3][3] = 1;
-    int winner = parseBoard(board, 6, 7, winPositions);
+    int winner = parseBoard(board, M, N, winPositions);
     TEST_CHECK(winner == 1);
 }
 
 void test_draw(void) {
-    int board[6][7];
-    bool winPositions[6][7] = {false};
+    int board[M][N];
+    bool winPositions[M][N] = {false};
 
-    for (int m = 0; m < 6; m++) {
-        for (int n = 0; n < 7; n++) {
+    for (int m = 0; m < M; m++) {
+        for (int n = 0; n < N; n++) {
             board[m][n] = (m + n) % 2 + 1; 
         }
     }
 
-    int winner = parseBoard(board, 6, 7, winPositions);
+    int winner = parseBoard(board, M, N, winPositions);
     TEST_CHECK(winner == 0); 
 }
 
